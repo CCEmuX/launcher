14a52212ad07aede3a044674b4703d471a95d553
Jonathan Coates <git@squiddev.cc>
2023-01-18 19:57:44 +0000

    Check in an editorconfig file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,28 @@
 plugins {
-    application
-    id("org.jetbrains.kotlin.jvm") version "1.8.0"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+	application
+	id("org.jetbrains.kotlin.jvm") version "1.8.0"
+	id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "cc.emux"
 version = "1.0"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib")
 }
 
 application {
-    mainClass.set("cc.emux.launcher.Launcher")
+	mainClass.set("cc.emux.launcher.Launcher")
 }
 
 tasks.compileKotlin { kotlinOptions.jvmTarget = "1.8" }
 tasks.compileTestKotlin { kotlinOptions.jvmTarget = "1.8" }
 
 tasks.shadowJar {
-    minimize()
-    exclude("**/*.kotlin_metadata")
+	minimize()
+	exclude("**/*.kotlin_metadata")
 }
